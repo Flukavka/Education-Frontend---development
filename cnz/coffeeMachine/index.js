481c669498f4d932bbf85e94ed0d97b4092ec1de
Victoriia <nosareva.vs@gmail.com>
2022-08-31 11:19:25 +0300

    Implemented immersion of banknotes in the bill acceptor

--- cnz/coffeeMachine/index.js
+++ cnz/coffeeMachine/index.js
@@ -4,7 +4,9 @@ let moneyEl = document.getElementById('money');
 let displayEl = document.querySelector('.display');
 let coin = null;
 const progressBarEl = document.querySelector('.progressBar');
-let rubFiftyEl = document.getElementById('rubFifty');
+let bills = document.querySelectorAll("img[src$='rub.jpg']");
+let billAccEl = document.querySelector('.billAcc');
+
 
 /**
  * Функция удаляет линию прогресса в прогресс-баре
@@ -59,22 +61,24 @@ function getChange(num) {
         num -= 1
     }
 };
-
 /* getChange(); */
+
+
 
 /**
  * Функция отключает встроенный drag and drop для изображений
  * @returns boolean false
  */
-rubFiftyEl.ondragstart = function () {
+/* rubFiftyEl.ondragstart = function () {
     return false;
-};
+}; */
+
 
 
 /**
  * Функция осуществляет drag and drop для изображения 50 руб. купюры
  */
-rubFiftyEl.onmousedown = function () {
+/* rubFiftyEl.onmousedown = function () {
     document.addEventListener('mousemove', moveElement);
     document.onmouseup = function () {
         document.removeEventListener('mousemove', moveElement);
@@ -85,10 +89,75 @@ rubFiftyEl.onmousedown = function () {
         let y = event.clientY - 62;
         rubFiftyEl.style.top = y + 'px';
         rubFiftyEl.style.left = x + 'px';
-        console.log(x)
-        console.log(y)
     };
+}; */
+
+
+
+for (let bill of bills) {
+    bill.onmousedown = function () {
+        bill.style.position = 'absolute';
+        bill.style.zIndex = 100;
+        bill.style.transform = 'rotate(90deg)';
+
+        document.addEventListener('mousemove', moveElement);
+
+        bill.onmouseup = function () {
+            bill.style.zIndex = 1; //не факт, что это логично
+            document.removeEventListener('mousemove', moveElement);
+
+            //координаты купюр
+            let billTop = bill.getBoundingClientRect().top;
+            let billLeft = bill.getBoundingClientRect().left;
+            let billRight = bill.getBoundingClientRect().right;
+
+            //координаты купюроприемника
+            let billAccTop = billAccEl.getBoundingClientRect().top;
+            let billAccLeft = billAccEl.getBoundingClientRect().left;
+            let billAccRight = billAccEl.getBoundingClientRect().right;
+            let billAccBottom = billAccEl.getBoundingClientRect().bottom -
+                (billAccEl.getBoundingClientRect().height / 3) * 2;
+
+            if (billTop > billAccTop &&
+                billLeft > billAccLeft &&
+                billRight < billAccRight &&
+                billTop < billAccBottom) {
+                bill.hidden = true;
+                moneyEl.value = +moneyEl.value + +bill.dataset.billValue; //NaN????
+            }
+        };
+
+        function moveElement(event) {
+            let x = event.clientX - 148;
+            let y = event.clientY - 62;
+            bill.style.top = y + 'px';
+            bill.style.left = x + 'px';
+        }
+    };
+
+    bill.ondragstart = function () {
+        return false;
+    }
 };
+
+
+
+
+
+
+
+
+/* for (i = 0; i < bills.length; i++) {
+    bills[i].onmousedown = function () {
+        console.log('click');
+    }
+} */    //dont work
+
+/* bills.forEach(function (num) {
+    num.onmousedown = function () {
+        console.log('click');
+    }
+}); */
 
 
 
