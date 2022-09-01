124a2bd8679c53bbdbfd8212530c501645261ba4
Victoriia <nosareva.vs@gmail.com>
2022-09-01 16:47:14 +0300

    before changing the variable display to balance

--- cnz/coffeeMachine/index.js
+++ cnz/coffeeMachine/index.js
@@ -1,21 +1,13 @@
 'use strict'
 
 let moneyEl = document.getElementById('money');
-let displayEl = document.querySelector('.display');
+let displayInfoEl = document.querySelector('.displayInfo');
 let coin = null;
-const progressBarEl = document.querySelector('.progressBar');
 let bills = document.querySelectorAll("img[src$='rub.jpg']");
 let billAccEl = document.querySelector('.billAcc');
-
-
-/**
- * Функция удаляет линию прогресса в прогресс-баре
- */
-function removeProgressLine() {
-    const progressBarLineEl = document.getElementById('progressLine');
-    progressBarLineEl.remove();
-    /*     turnOnBtn(); */
-};
+let changeBoxEl = document.querySelector('.changeBox');
+let progressBar = document.querySelector('.progress-bar');
+let balanceEl = document.querySelector('.balance')
 
 
 /**
@@ -26,17 +18,33 @@ function removeProgressLine() {
  * @param {string} name наименование продукта
  */
 function getCoffee(price, name) {
-    /* disabledBtn(); */
     if (moneyEl.value >= price) {
-        progressBarEl.insertAdjacentHTML('afterbegin', '<div id="progressLine" class="progressBarLine" style="max-width: 100%;"></div>');
         moneyEl.value = moneyEl.value - price;
-        setTimeout((displayEl.innerHTML = 'Ваш ' + name + ' готов!'), 4500);
-        setTimeout(() => removeProgressLine(), 3500);
+        displayInfoEl.innerHTML = `Баланс: ${moneyEl.value} руб`;
+        startProgressBar(name)
     } else {
-        displayEl.innerHTML = 'На ' + name + ' недостаточно денег!';
+        displayInfoEl.innerHTML = 'На ' + name + ' недостаточно денег!';
     }
 };
 
+function startProgressBar(coffeeName) {
+    let i = 0;
+    displayInfoEl.innerHTML = `<i class="bi bi-hourglass"></i> ${coffeeName} готовится...`;
+    function progress() {
+        progressBar.parentElement.hidden = false;
+        i++;
+        progressBar.style.width = i + '%';
+
+        if (i == 100) {
+            progressBar.parentElement.hidden = true;
+            displayInfoEl.innerHTML = `<i class="bi bi-hourglass-bottom"></i> Ваш ${coffeeName} готов! <br> Баланс: ${moneyEl.value} руб`;
+            clearInterval(timerId)
+        } else if (i == 45) {
+            displayInfoEl.innerHTML = `<i class="bi bi-hourglass-split"></i> ${coffeeName} почти готов`;
+        }
+    };
+    let timerId = setInterval(progress, 100);
+};
 
 /**
  * Функция выводит в консоль значение суммы в монетах номиналом: 10, 5, 2, 1
@@ -44,54 +52,34 @@ function getCoffee(price, name) {
  * @param {number} num принимает значение, переданное при вызове функции
  */
 function getChange(num) {
-    for (coin = 10; num >= 10;) {
-        num -= coin;
-        console.log(coin);
-    }
-    for (coin = 5; num >= 5;) {
-        num -= coin;
-        console.log(coin)
-    }
-    for (coin = 2; num >= 2;) {
-        num -= coin;
-        console.log(coin)
-    }
-    for (coin = 1; num >= 1;) {
-        console.log(num);
-        num -= 1
-    }
+    while (num != 0) {
+        let top = getRandom(50, changeBoxEl.getBoundingClientRect().height - 75);
+        let left = getRandom(50, changeBoxEl.getBoundingClientRect().width - 75);
+        if (num >= 10) {
+            coin = 10;
+            num -= coin;
+            changeBoxEl.innerHTML += `<img src="image/10rub.png" style="top: ${top}px;
+            left: ${left}px;" />`;
+        } else if (num >= 5) {
+            coin = 5;
+            num -= coin;
+            changeBoxEl.innerHTML += `<img src="image/5rub.png" style="top: ${top}px;
+            left: ${left}px;" />`;
+        } else if (num >= 2) {
+            coin = 2;
+            num -= coin;
+            changeBoxEl.innerHTML += `<img src="image/2rub.png" style="top: ${top}px;
+            left: ${left}px;" />`;
+        } else {
+            coin = 1;
+            num -= 1;
+            changeBoxEl.innerHTML += `<img src="image/1rub.png" style="top: ${top}px;
+            left: ${left}px;" />`;
+        }
+    };
+    moneyEl.value = 0;
+    displayInfoEl.innerHTML = `Баланс: ${moneyEl.value} руб`; //dont work
 };
-/* getChange(); */
-
-
-
-/**
- * Функция отключает встроенный drag and drop для изображений
- * @returns boolean false
- */
-/* rubFiftyEl.ondragstart = function () {
-    return false;
-}; */
-
-
-
-/**
- * Функция осуществляет drag and drop для изображения 50 руб. купюры
- */
-/* rubFiftyEl.onmousedown = function () {
-    document.addEventListener('mousemove', moveElement);
-    document.onmouseup = function () {
-        document.removeEventListener('mousemove', moveElement);
-    };
-
-    function moveElement(event) {
-        let x = event.clientX - 148;
-        let y = event.clientY - 62;
-        rubFiftyEl.style.top = y + 'px';
-        rubFiftyEl.style.left = x + 'px';
-    };
-}; */
-
 
 
 for (let bill of bills) {
@@ -122,8 +110,13 @@ for (let bill of bills) {
                 billLeft > billAccLeft &&
                 billRight < billAccRight &&
                 billTop < billAccBottom) {
-                bill.hidden = true;
-                moneyEl.value = +moneyEl.value + +bill.dataset.billValue; //NaN????
+                bill.classList.add('hideBill')
+                setTimeout(function () {
+                    bill.hidden = true;
+                }, 245);
+
+                moneyEl.value = +moneyEl.value + +bill.dataset.billValue;
+                displayInfoEl.innerHTML = `Баланс: ${moneyEl.value} руб`;
             }
         };
 
@@ -141,8 +134,44 @@ for (let bill of bills) {
 };
 
 
+/**
+ * Функция генерирует рандомное число в переданном диапазоне
+ * @param {number} min минимальная граница диапазона
+ * @param {number} max максимальная граница диапазона
+ * @returns возвращает рандомное число
+ */
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
 
+
+/**
+ * Функция отключает встроенный drag and drop для изображений
+ * @returns boolean false
+ */
+/* rubFiftyEl.ondragstart = function () {
+    return false;
+}; */
+
+
+
+/**
+ * Функция осуществляет drag and drop для изображения 50 руб. купюры
+ */
+/* rubFiftyEl.onmousedown = function () {
+    document.addEventListener('mousemove', moveElement);
+    document.onmouseup = function () {
+        document.removeEventListener('mousemove', moveElement);
+    };
+
+    function moveElement(event) {
+        let x = event.clientX - 148;
+        let y = event.clientY - 62;
+        rubFiftyEl.style.top = y + 'px';
+        rubFiftyEl.style.left = x + 'px';
+    };
+}; */
 
 
 
